6cfa1c5836410096638510676af4ea54cc47bdb8
Vadim Uchaev <42405830+vuchaev2015@users.noreply.github.com>
2023-06-14 00:54:49 +0300

    added new method "friends" for check

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import requests
 import time
 import random
 from loguru import logger
-from threading import Thread, Semaphore
+from threading import Thread
 import argparse
+import json
 
 def update_username_list(username):
     with open('usernames.txt', 'r') as f:
@@ -12,29 +13,46 @@ def update_username_list(username):
         usernames.remove(username)
         with open('usernames.txt', 'w') as f:
             f.write('\n'.join(usernames))
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+method = config['method']
 def check_username(username, token, headers):
     try:
-        json_data = {'username': username, 'password': ''}
-        response = requests.patch('https://discord.com/api/v9/users/@me', headers=headers, json=json_data)
-        response_json = response.json()
 
+        json_data = {'username': username}
+        if method == "friends":
+            response = requests.post('https://discord.com/api/v9/users/@me/pomelo-attempt', headers=headers, json=json_data)
+        else:
+            response = requests.patch('https://discord.com/api/v9/users/@me', headers=headers, json=json_data)
+        response_json = response.json()
+        print(response.json())
         if response.status_code == 400 and response_json['message'] == 'Invalid Form Body':
             errors = response_json['errors']
             if 'username' in errors:
                 error_codes = [error['code'] for error in errors['username']['_errors']]
                 if 'USERNAME_ALREADY_TAKEN' in error_codes:
                     return 'taken'
-            elif "PASSWORD_DOES_NOT_MATCH" in str(errors):
+            elif "PASSWORD_DOES_NOT_MATCH" in str(errors) or response_json["taken"] is not True:
+                return 'not_taken'
+        elif response.status_code == 200 and 'taken' in response_json:
+            if response_json["taken"] is not False:
+                return 'taken'
+            elif response_json["taken"] is not True:
                 return 'not_taken'
         elif response.status_code == 401:
-            return 'unauthorized'
+            if response_json.get('code') == 40001:
+                return '40001'
+            else:
+                return 'unauthorized'
+
         elif "retry_after" in response_json:
             return 'rate_limited', response_json["retry_after"]
         else:
-            return 'unknown_error'
+            return 'unknown_error', response_json
     except requests.exceptions.RequestException as e:
         return 'connection_error'
-
 def load_file(file_name):
     with open(file_name, 'r') as f:
         return [line.strip() for line in f if line.strip()]
@@ -85,6 +103,7 @@ class Worker(Thread):
         self.tokens = tokens
         self.usernames_queue = usernames_queue
         self.lock = lock
+        self.checked_usernames = []
 
     def get_headers(self, token):
         headers = {
@@ -104,11 +123,11 @@ class Worker(Thread):
                 if self.usernames_queue.empty():
                     break
                 username = self.usernames_queue.get()
+
                 best_token = get_best_token(self.tokens)
                 if best_token is None:
                     logger.warning(f"Поток {self.name}: Нет доступных токенов. Завершаем поток.")
                     return
-                best_token.set_in_use(True)
 
             while time.time() < best_token.get_sleep_until():
                 time.sleep(0.1)
@@ -119,17 +138,22 @@ class Worker(Thread):
             if result == 'taken':
                 logger.info(f'Поток {self.name}: Имя пользователя {username} уже используется.')
                 append_to_file('bad.txt', username)
-                update_username_list(username)
                 best_token.set_sleep_until(time.time() + random.uniform(4, 6))
             elif result == 'not_taken':
                 logger.info(f'Поток {self.name}: Имя пользователя {username} свободно.')
                 append_to_file('good.txt', username)
-                update_username_list(username)
                 best_token.set_sleep_until(time.time() + random.uniform(4, 6))
 
             elif result == 'connection_error':
                 logger.warning(f"Поток {self.name}: Ошибка соединения. Повторяем запрос через 10 секунд.")
                 best_token.set_sleep_until(time.time() + 10)
+
+            elif result == '40001':
+                if method == "friends":
+                    logger.warning(
+                        f"Поток {self.name}: Токен {best_token.token} не подходит для параметра friends и был удален")
+                    self.tokens.remove(best_token)
+                    best_token.set_sleep_until(time.time() + 10)
 
             elif result == 'unauthorized':
                 logger.warning(f"Поток {self.name}: Токен {best_token.token} является неавторизованным и был удален")
@@ -137,7 +161,8 @@ class Worker(Thread):
                 best_token.set_sleep_until(time.time() + 10)
 
             elif result == 'unknown_error':
-                logger.error(f"Unknown error: {result}")
+                error_json = result[1]
+                logger.error(f"Unknown error: {error_json}")
 
             elif result[0] == 'rate_limited':
                 sleep_time = result[1] + random.uniform(0.5, 1.2)
@@ -145,7 +170,10 @@ class Worker(Thread):
                     f"Поток {self.name}: Токен {best_token.token} Rate Limited. Спим {sleep_time} секунд перед следующим запросом.")
                 best_token.set_sleep_until(time.time() + sleep_time)
 
-
+            if result in ['taken', 'not_taken', 'unknown_error']:
+                update_username_list(username)
+            else:
+                self.usernames_queue.put(username)
             best_token.set_in_use(False)
 
 def main():
@@ -174,17 +202,21 @@ def main():
 
         while connection_error:
             try:
-                response = requests.patch('https://discord.com/api/v9/users/@me', headers=headers,
-                                          json={'username': 'qwe', 'password': ''})
+                if method == "friends":
+                    response = requests.post('https://discord.com/api/v9/users/@me/pomelo-attempt', headers=headers,
+                                              json={'username': ''})
+                else:
+                    response = requests.patch('https://discord.com/api/v9/users/@me', headers=headers,
+                                              json={'username': ''})
+
                 response_json = response.json()
                 connection_error = False
 
                 if 'retry_after' in response_json:
                     logger.error(f"Токен {token} Rate limited")
-                    print(response_json)
                 elif response.status_code == 401:
                     logger.error(f"Токен {token} является неавторизованным и был удален")
-                elif 'USERNAME_ALREADY_TAKEN' in str(response_json):
+                elif 'USERNAME_ALREADY_TAKEN' in str(response_json) or 'BASE_TYPE_BAD_LENGTH' in str(response_json):
                     logger.success(f"Токен {token} готов к работе.")
                     valid_tokens.append(Token(token))
                 elif 'USERNAME_TOO_MANY_USERS' in str(response_json):
